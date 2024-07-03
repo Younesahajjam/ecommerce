d16f6900e5de4d8637d9d7ea748cc72edcb6e6c0
younesahajjam <younesahajjam3@gmail.com>
2024-07-03 23:16:39 +0100

    change api url

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL = "http://localhost:5757";
+export const API_BASE_URL = "https://ecommerce-server-production-6557.up.railway.app";
 
 const jwt = localStorage.getItem("jwt");
 //const cors=require("cors");
